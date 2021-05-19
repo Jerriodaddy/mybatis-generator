29199d418f15f3368381d3a9f99d9d4e7224a324
Zhirui <jerrio20@outlook.com>
2021-05-19 22:03:12 +0800

    aa

---
+++ src/main/java/com/jerrio/mbg/mapper/ClassMapper.java
@@ -0,0 +1,7 @@
+package com.jerrio.mbg.mapper;
+
+import com.jerrio.mbg.pojo.Class;
+import com.jerrio.mbg.utils.MyMapper;
+
+public interface ClassMapper extends MyMapper<Class> {
+}
---
+++ src/main/java/com/jerrio/mbg/pojo/User.java
@@ -0,0 +1,72 @@
+package com.jerrio.mbg.pojo;
+
+import javax.persistence.*;
+
+@Table(name = "`user`")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    private String age;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
